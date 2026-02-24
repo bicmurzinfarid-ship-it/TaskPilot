c287daf8675b337197f15e20f485fde2069bfba3
bicmu <bicmurzinfarid@gmail.com>
2026-02-25 02:48:45 +0400

    base commit

---
+++ src/test/java/TaskPilot/pre/TaskPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskPilot.pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
